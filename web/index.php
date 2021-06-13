63dd763dd29645c66eff2ece0ac792433978e34b
djhampson <david@firstbytecomputers.net>
2021-06-13 20:35:16 +1000

    added database access

--- web/index.php
+++ web/index.php
@@ -45,5 +45,21 @@ $app->get('/cowsay', function() use($app) {
 });
 
 
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
+
 
 $app->run();

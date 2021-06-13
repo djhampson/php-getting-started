110ba59c5427a022a1c60cb15d001f66b274573c
djhampson <david@firstbytecomputers.net>
2021-06-13 20:29:26 +1000

    Demo

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@ $app->get('/', function() use($app) {
 
 $app->get('/cowsay', function() use($app) {
   $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+  return "<pre>".\Cowsayphp\Cow::say("Howdy")."</pre>";
 });
 
 
